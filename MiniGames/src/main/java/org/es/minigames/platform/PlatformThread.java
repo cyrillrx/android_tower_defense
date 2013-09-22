6bfcbe603707d4660511da6bac589e426d3f91f3
Cyril Leroux <cyril0leroux@gmail.com>
2013-09-22 20:32:01 +0200

    Prepared platform game structure.

---
+++ MiniGames/src/main/java/org/es/minigames/platform/PlatformThread.java
@@ -0,0 +1,32 @@
+package org.es.minigames.platform;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+import org.es.minigames.common.DrawingThread;
+
+/**
+ * Created by Cyril on 22/09/13.
+ */
+public class PlatformThread extends DrawingThread {
+
+    public PlatformThread(SurfaceHolder surfaceHolder, Context context) {
+        super(surfaceHolder, context);
+    }
+
+    @Override
+    protected void updateSurfaceSize() {
+
+    }
+
+    @Override
+    protected void update() {
+
+    }
+
+    @Override
+    protected void doDraw(Canvas canvas) {
+
+    }
+}
---
+++ MiniGames/src/main/java/org/es/minigames/platform/PlatformView.java
@@ -0,0 +1,16 @@
+package org.es.minigames.platform;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+/**
+ * Created by Cyril on 22/09/13.
+ */
+public class PlatformView extends SurfaceView {
+
+    public PlatformView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
