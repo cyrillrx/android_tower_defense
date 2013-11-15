de496bcf224ee9cd6ddcfb87b9c9fc93fcef3400
Cyril Leroux <cyril.leroux@cgi.com>
2013-11-15 16:42:55 +0100

    Implemented GooglePlay Games Sign-In button

--- MiniGames/src/main/java/org/es/minigames/MainActivity.java
+++ MiniGames/src/main/java/org/es/minigames/MainActivity.java
@@ -11,15 +11,29 @@ import org.es.minigames.platform.PlatformActivity;
 import org.es.minigames.scrollingbackgrounds.ScrollingBackgroundActivity;
 
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends BaseGameActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((Button) findViewById(R.id.btn_scrolling_bg)).setOnClickListener(this);
-        ((Button) findViewById(R.id.btn_platform)).setOnClickListener(this);
+        findViewById(R.id.btn_scrolling_bg).setOnClickListener(this);
+        findViewById(R.id.btn_platform).setOnClickListener(this);
+        findViewById(R.id.button_sign_in).setOnClickListener(this);
+        findViewById(R.id.button_sign_out).setOnClickListener(this);
+    }
+
+    // Shows the "sign in" bar (explanation and button).
+    private void showSignInBar() {
+        findViewById(R.id.sign_in_bar).setVisibility(View.VISIBLE);
+        findViewById(R.id.sign_out_bar).setVisibility(View.GONE);
+    }
+
+    // Shows the "sign out" bar (explanation and button).
+    private void showSignOutBar() {
+        findViewById(R.id.sign_in_bar).setVisibility(View.GONE);
+        findViewById(R.id.sign_out_bar).setVisibility(View.VISIBLE);
     }
 
 //    @Override
@@ -27,6 +41,30 @@ public class MainActivity extends Activity implements View.OnClickListener {
 //        getMenuInflater().inflate(R.menu.scrolling_bg, menu);
 //        return true;
 //    }
+
+    /**
+     * Called to notify us that sign in failed. Notice that a failure in sign in is not
+     * necessarily due to an error; it might be that the user never signed in, so our
+     * attempt to automatically sign in fails because the user has not gone through
+     * the authorization flow. So our reaction to sign in failure is to show the sign in
+     * button. When the user clicks that button, the sign in process will start/resume.
+     */
+    @Override
+    public void onSignInFailed() {
+        // Sign-in has failed. So show the user the sign-in button
+        // so they can click the "Sign-in" button.
+        showSignInBar();
+    }
+
+    /**
+     * Called to notify us that sign in succeeded. We react by loading the loot from the
+     * cloud and updating the UI to show a sign-out button.
+     */
+    @Override
+    public void onSignInSucceeded() {
+        // Sign-in worked!
+        showSignOutBar();
+    }
 
     @Override
     public void onClick(View v) {
@@ -40,6 +78,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
             case R.id.btn_platform:
                 startActivity(new Intent(getApplicationContext(), PlatformActivity.class));
                 break;
+
+            case R.id.button_sign_in:
+                // start the sign-in flow
+                beginUserInitiatedSignIn();
+                break;
+
+            case R.id.button_sign_out:
+                // sign out.
+                signOut();
+                showSignInBar();
+                break;
+
         }
     }
 }
