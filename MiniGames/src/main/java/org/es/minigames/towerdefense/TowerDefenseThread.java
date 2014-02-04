87551bea808ef173d037a003a64dfb60a39c8ab1
Cyril Leroux <cyril0leroux@gmail.com>
2014-02-04 23:49:09 +0100

    TowerDefense : Created the basics for TowerFactory and TowerDefenseThread.

--- MiniGames/src/main/java/org/es/minigames/towerdefense/TowerDefenseThread.java
+++ MiniGames/src/main/java/org/es/minigames/towerdefense/TowerDefenseThread.java
@@ -6,6 +6,8 @@ import android.view.SurfaceHolder;
 
 import org.es.gameengine.DrawingThread;
 import org.es.gameengine.UserEvent;
+import org.es.minigames.towerdefense.drawable.tower.Tower;
+import org.es.minigames.towerdefense.drawable.tower.TowerFactory;
 
 /**
  * @author Cyril Leroux
@@ -13,8 +15,13 @@ import org.es.gameengine.UserEvent;
  */
 public class TowerDefenseThread extends DrawingThread {
 
+    Tower[] mTowers = new Tower[1];
+
     public TowerDefenseThread(SurfaceHolder surfaceHolder, Context context) {
         super(surfaceHolder, context);
+
+        mTowers[0] = TowerFactory.createBasicTower(context.getResources());
+
     }
 
     @Override
@@ -35,5 +42,8 @@ public class TowerDefenseThread extends DrawingThread {
     @Override
     protected void doDraw(Canvas canvas) {
 
+        for (Tower tower : mTowers) {
+            tower.draw(canvas);
+        }
     }
 }
--- MiniGames/src/main/java/org/es/minigames/towerdefense/drawable/tower/TowerFactory.java
+++ MiniGames/src/main/java/org/es/minigames/towerdefense/drawable/tower/TowerFactory.java
@@ -1,9 +1,11 @@
 package org.es.minigames.towerdefense.drawable.tower;
 
 import android.content.res.Resources;
+import android.graphics.Rect;
 
 import org.es.gameengine.drawable.Animation;
 import org.es.gameengine.drawable.BitmapAnimation;
+import org.es.gameengine.drawable.SpriteSheetAnimation;
 import org.es.minigames.R;
 
 import java.util.ArrayList;
@@ -14,10 +16,7 @@ import java.util.List;
  */
 public class TowerFactory {
 
-    private static final int[] RES_BASIC_TOWER_LEFT  = new int[] { R.drawable.hero_left_1 };
-    private static final int[] RES_BASIC_TOWER_RIGHT = new int[] { R.drawable.hero_left_1 };
-    private static final int[] RES_BASIC_TOWER_UP    = new int[] { R.drawable.hero_left_1 };
-    private static final int[] RES_BASIC_TOWER_DOWN  = new int[] { R.drawable.hero_left_1 };
+    private static final int RES_BASIC_TOWER_SS  = R.drawable.tower_1_sprite_sheet;
     private static final int BASIC_TOWER_HEALTH = 200;
     private static final int BASIC_TOWER_DAMAGE = 20;
     private static final int BASIC_TOWER_ATTACK_RANGE  = 3;
@@ -27,10 +26,10 @@ public class TowerFactory {
     public static Tower createBasicTower(Resources resources) {
 
         List<Animation> mAnimations = new ArrayList<>();
-        mAnimations.add(Tower.DIRECTION_LEFT,   new BitmapAnimation(resources, RES_BASIC_TOWER_LEFT,  -1, false, null));
-        mAnimations.add(Tower.DIRECTION_RIGHT,  new BitmapAnimation(resources, RES_BASIC_TOWER_RIGHT, -1, false, null));
-        mAnimations.add(Tower.DIRECTION_UP,     new BitmapAnimation(resources, RES_BASIC_TOWER_UP,    -1, false, null));
-        mAnimations.add(Tower.DIRECTION_DOWN,   new BitmapAnimation(resources, RES_BASIC_TOWER_DOWN,  -1, false, null));
+        mAnimations.add(Tower.DIRECTION_LEFT,  new SpriteSheetAnimation(resources, RES_BASIC_TOWER_SS, new Rect[] { new Rect(300, 0,   450, 150) },  -1, false, null));
+        mAnimations.add(Tower.DIRECTION_RIGHT, new SpriteSheetAnimation(resources, RES_BASIC_TOWER_SS, new Rect[] { new Rect(300, 150, 450, 300) },  -1, false, null));
+        mAnimations.add(Tower.DIRECTION_UP ,   new SpriteSheetAnimation(resources, RES_BASIC_TOWER_SS, new Rect[] { new Rect(0,   150, 150, 300) },  -1, false, null));
+        mAnimations.add(Tower.DIRECTION_DOWN,  new SpriteSheetAnimation(resources, RES_BASIC_TOWER_SS, new Rect[] { new Rect(0,   0,   150, 150) },  -1, false, null));
 
         return createTower(mAnimations,
                 BASIC_TOWER_HEALTH,
