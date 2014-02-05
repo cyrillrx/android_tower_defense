68dae841751388d699be0cde61158609697bd0b7
Cyril Leroux <cyril0leroux@gmail.com>
2014-02-05 23:17:28 +0100

    TowerDefense : Created a WaveMgr class.

--- MiniGames/src/main/java/org/es/minigames/towerdefense/object/Wave.java
+++ MiniGames/src/main/java/org/es/minigames/towerdefense/object/Wave.java
@@ -5,13 +5,14 @@ import org.es.minigames.towerdefense.unit.Enemy;
 import java.util.Map;
 
 /**
- * Class that represents a wave of monsters.
+ * Class that represents a wave of attackers.
  *
  * @author Cyril Leroux
  *         Created on 30/01/14.
  */
 public class Wave {
 
-    private Map<Enemy.Type, Integer> mMonsterCount;
+    /** Number of attackers of each type to be spawn during the wave. */
+    private Map<Enemy.Type, Integer> mAttackers;
     private int mDuration;
 }
---
+++ MiniGames/src/main/java/org/es/minigames/towerdefense/process/WaveMgr.java
@@ -0,0 +1,8 @@
+package org.es.minigames.towerdefense.process;
+
+/**
+ * Created by Cyril Leroux on 05/02/14.
+ */
+public class WaveMgr {
+
+}
