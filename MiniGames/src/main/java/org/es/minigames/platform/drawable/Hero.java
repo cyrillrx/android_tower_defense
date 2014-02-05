fe521662c234ab0351d959cbf3e1e5eb2bfa312b
Cyril Leroux <cyril0leroux@gmail.com>
2014-02-05 23:05:43 +0100

    Platform : Updated Hero class (implements Sprite)

--- MiniGames/src/main/java/org/es/minigames/platform/drawable/Hero.java
+++ MiniGames/src/main/java/org/es/minigames/platform/drawable/Hero.java
@@ -8,19 +8,27 @@ import org.es.gameengine.AnimationCallback;
 import org.es.gameengine.drawable.Animation;
 import org.es.gameengine.drawable.Background;
 import org.es.gameengine.drawable.BitmapAnimation;
+import org.es.gameengine.drawable.GenericSprite;
 import org.es.gameengine.drawable.Sprite;
 import org.es.minigames.BuildConfig;
 import org.es.minigames.R;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /** Created by Cyril on 02/10/13. */
-public class Hero extends Sprite implements AnimationCallback{
+public class Hero implements Sprite, AnimationCallback {
 
     private static final String TAG = "Hero";
 
     // Hero states
-    private static final int STATE_STATIC   = 0;
-    private static final int STATE_WALKING  = 1;
-    private static final int STATE_JUMPING  = 2;
+
+    private static final int ANIM_WALK_LEFT = 0;
+    private static final int ANIM_WALK_RIGHT = 1;
+
+    private static final int STATE_STATIC = 0;
+    private static final int STATE_WALKING = 1;
+    private static final int STATE_JUMPING = 2;
 
     private static final float START_ACCELERATION_DELAY = 2000;
     /** Action duration to reach the maximum speed (in milliseconds). */
@@ -33,38 +41,44 @@ public class Hero extends Sprite implements AnimationCallback{
     private int mState = STATE_STATIC;
     /** Character current speed in pixels per second. */
     private float mCurrentSpeed = 0;
-//    private float mVelocityX = 0;
-//    private float mVelocityY = 0;
+    //    private float mVelocityX = 0;
+    //    private float mVelocityY = 0;
 
-    private Background mBackground;
-    private Animation mWalkLeft = null;
-    private Animation mWalkRight = null;
+    private final GenericSprite mSprite;
+    private final Background mBackground;
 
     public Hero(Resources resources, Background background) {
-        super(null);
-
+        mSprite = new GenericSprite(getAnimations(resources, this));
+        mSprite.setAnimationId(ANIM_WALK_LEFT);
         mBackground = background;
-        mWalkLeft = new BitmapAnimation(resources,
-                new int[] {
+
+        mState = STATE_WALKING;
+    }
+
+    private static Map<Integer, Animation> getAnimations(Resources resources, AnimationCallback callback) {
+
+        Map<Integer, Animation> animations = new HashMap<>();
+
+        animations.put(ANIM_WALK_LEFT, new BitmapAnimation(resources,
+                new int[]{
                         R.drawable.hero_left_1,
                         R.drawable.hero_left_2,
                         R.drawable.hero_left_3,
                         R.drawable.hero_left_4,
                         R.drawable.hero_left_5,
                         R.drawable.hero_left_6,
-                }, 150, true, this);
+                }, 150, true, callback));
 
-        mWalkRight = new BitmapAnimation(resources,
-                new int[] {
+        animations.put(ANIM_WALK_RIGHT, new BitmapAnimation(resources,
+                new int[]{
                         R.drawable.hero_right_1,
                         R.drawable.hero_right_2,
                         R.drawable.hero_right_3,
                         R.drawable.hero_right_4,
                         R.drawable.hero_right_5,
                         R.drawable.hero_right_6,
-                }, 150, true, this);
-
-        mAnimation = mWalkRight;
+                }, 150, true, callback));
+        return animations;
     }
 
     public boolean update() {
@@ -72,7 +86,7 @@ public class Hero extends Sprite implements AnimationCallback{
         boolean updated = false;
 
         updateSpeed();
-        updated |= mAnimation.updateFrame();
+        updated |= getAnimation().updateFrame();
         updated |= updatePosition();
 
         return updated;
@@ -80,7 +94,7 @@ public class Hero extends Sprite implements AnimationCallback{
 
     private void updateSpeed() {
         if (mState == STATE_WALKING) {
-            final float actionElapsedTime = Math.min(System.currentTimeMillis() - mAnimation.getStartTime(), REACH_MAX_SPEED_DELAY);
+            final float actionElapsedTime = Math.min(System.currentTimeMillis() - getAnimation().getStartTime(), REACH_MAX_SPEED_DELAY);
             if (actionElapsedTime > START_ACCELERATION_DELAY) {
                 // speed that will be added to the standard walking speed
                 final float extraSpeed = actionElapsedTime * (MAX_SPEED - WALKING_SPEED) / REACH_MAX_SPEED_DELAY;
@@ -93,12 +107,12 @@ public class Hero extends Sprite implements AnimationCallback{
     }
 
     private boolean updatePosition() {
-        if (mAnimation.isRunning()) {
-            if (mWalkLeft.equals(mAnimation)) {
+        if (getAnimation().isRunning()) {
+            if (ANIM_WALK_LEFT == getAnimationId()) {
                 mBackground.setScrollSpeed(mCurrentSpeed, 1000);
 
-            } else if (mWalkRight.equals(mAnimation)) {
-                mBackground.setScrollSpeed(-1*mCurrentSpeed, 1000);
+            } else if (ANIM_WALK_RIGHT == getAnimationId()) {
+                mBackground.setScrollSpeed(-1 * mCurrentSpeed, 1000);
             }
         } else {
             mBackground.setScrollSpeed(0, 1000);
@@ -107,16 +121,40 @@ public class Hero extends Sprite implements AnimationCallback{
         return true;
     }
 
+
+    @Override
+    public void onUpdateSurfaceSize(int surfaceWidth, int surfaceHeight) {
+        mSprite.onUpdateSurfaceSize(surfaceWidth, surfaceHeight);
+    }
+
     @Override
     public void draw(Canvas canvas) {
-        if (mAnimation != null) {
-            mAnimation.draw(canvas, mPosition);
-        }
+        mSprite.draw(canvas);
+    }
+
+    @Override
+    public void startAnimation() {
+        mSprite.startAnimation();
     }
 
     @Override
     public void stopAnimation() {
-        super.stopAnimation();
+        mSprite.stopAnimation();
+    }
+
+    @Override
+    public int getAnimationId() {
+        return mSprite.getAnimationId();
+    }
+
+    @Override
+    public Animation getAnimation() {
+        return mSprite.getAnimation();
+    }
+
+    @Override
+    public void setAnimationId(final int id) {
+        mSprite.setAnimationId(id);
     }
 
     @Override
@@ -126,17 +164,17 @@ public class Hero extends Sprite implements AnimationCallback{
     }
 
     /** Change the animation if necessary. */
-    protected void switchAnimation(Animation animation) {
-        if (animation == null || animation.equals(mAnimation)) {
+    protected void switchAnimation(int animationId) {
+        if (animationId == mSprite.getAnimationId()) {
             return;
         }
-        mAnimation = animation;
+        setAnimationId(animationId);
     }
 
     public void walkLeft() {
         mState = STATE_WALKING;
-        switchAnimation(mWalkLeft);
-        if (!mAnimation.isRunning()) {
+        switchAnimation(ANIM_WALK_LEFT);
+        if (!getAnimation().isRunning()) {
             mCurrentSpeed = WALKING_SPEED;
             startAnimation();
         }
@@ -144,8 +182,8 @@ public class Hero extends Sprite implements AnimationCallback{
 
     public void walkRight() {
         mState = STATE_WALKING;
-        switchAnimation(mWalkRight);
-        if (!mAnimation.isRunning()) {
+        switchAnimation(ANIM_WALK_RIGHT);
+        if (!getAnimation().isRunning()) {
             mCurrentSpeed = WALKING_SPEED;
             startAnimation();
         }
@@ -154,7 +192,7 @@ public class Hero extends Sprite implements AnimationCallback{
     public void jump() {
         mState = STATE_JUMPING;
         // switchAnimation();
-//        xt = vx * t;
-//        yt = vy * t - (g * t²)/2;
+        //        xt = vx * t;
+        //        yt = vy * t - (g * t²)/2;
     }
 }
