627afb9880cc76b649ef5cdf8f2ec57c2508dd21
Cyril Leroux <cyril0leroux@gmail.com>
2014-02-18 02:23:23 +0100

    TowerDefense : Created a TileFactory and added tile backgrounds.

--- MiniGames/src/main/java/org/es/minigames/towerdefense/battleground/Tile.java
+++ MiniGames/src/main/java/org/es/minigames/towerdefense/battleground/Tile.java
@@ -1,5 +1,6 @@
 package org.es.minigames.towerdefense.battleground;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -16,10 +17,20 @@ import org.es.minigames.towerdefense.unit.AbstractUnit;
  */
 public class Tile implements DrawableElement {
 
-    private final RectF mBoundingRect;
-    private AbstractUnit mBindedUnit = null;
+    public static enum Type {
+        GRASS, EARTH, SNOW, SAND
+    }
 
-    public Tile() {
+    private final Bitmap mBackground;
+    private final int mPosX;
+    private final int mPosY;
+    private final RectF mBoundingRect;
+    private AbstractUnit mBoundUnit = null;
+
+    public Tile(int columnId, int rowId, Bitmap background) {
+        mPosX = columnId;
+        mPosY = rowId;
+        mBackground = background;
         mBoundingRect = new RectF();
     }
 
@@ -29,6 +40,9 @@ public class Tile implements DrawableElement {
     @Override
     public void draw(Canvas canvas) {
 
+        // Draw the background
+        canvas.drawBitmap(mBackground, null, getRect(), null);
+
         // Draw the grid
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setAntiAlias(true);
@@ -36,27 +50,58 @@ public class Tile implements DrawableElement {
         paint.setColor(Color.BLUE);
         paint.setStyle(Paint.Style.STROKE);
         canvas.drawRect(getRect(), paint);
-
-        if (mBindedUnit != null) {
-            mBindedUnit.draw(canvas);
-        }
     }
 
+    //
+    // Grid positions
+    //
+
+    /** @return The position on the grid. */
+    @Override
+    public float getPosX() { return mPosX; }
+
+    /** @return The position on the grid. */
+    @Override
+    public float getPosY() { return mPosY; }
+
+    /** Do nothing. The position of the tile is immutable. */
+    @Override
+    public void setPosition(float x, float y) { }
+
+    @Override
+    public float getWidth() { return 1f; }
+
+    @Override
+    public float getHeight() { return 1f; }
+
+    /** @return The centerX position on the grid. */
+    public float getCenterX() { return mPosX + getWidth() / 2f; }
+
+    /** @return The centerY position on the grid. */
+    public float getCenterY() { return mPosY + getHeight() / 2f; }
+
+    //
+    // position on screen
+    //
+
+    /** @return The rect on screen. */
     public RectF getRect() { return mBoundingRect; }
 
+    /** Set the rect on screen. */
     public void setRect(float left, float top, float right, float bottom) {
         mBoundingRect.set(left, top, right, bottom);
     }
 
-    public float centerX() { return mBoundingRect.centerX(); }
+    //
+    // Other
+    //
 
-    public float centerY() { return mBoundingRect.centerY(); }
+    public boolean isEmpty() { return mBoundUnit == null; }
 
-    public boolean isEmpty() { return mBindedUnit == null; }
+    /** @return True if the tile is available for building. */
+    public boolean isBuildingLand() { return mBoundUnit == null; }
 
-    public boolean isBuildable() { return mBindedUnit == null; }
+    public AbstractUnit getBoundUnit() { return mBoundUnit; }
 
-    public AbstractUnit getBindedUnit() { return mBindedUnit; }
-
-    public void bindUnit(AbstractUnit unit) { mBindedUnit = unit; }
+    public void bindUnit(AbstractUnit unit) { mBoundUnit = unit; }
 }
