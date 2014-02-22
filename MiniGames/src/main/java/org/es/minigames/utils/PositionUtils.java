653bb2b3a80d02d7062e3dd557934a107760a1a9
Cyril Leroux <cyril0leroux@gmail.com>
2014-02-22 11:27:51 +0100

    Utils : Updated  PositionUtils

--- MiniGames/src/main/java/org/es/minigames/utils/PositionUtils.java
+++ MiniGames/src/main/java/org/es/minigames/utils/PositionUtils.java
@@ -8,6 +8,13 @@ import android.graphics.RectF;
  */
 public class PositionUtils {
 
+    /**
+     * This method indicates whether a point is within a rectangle or not.
+     *
+     * @param rect The rectangle in which to look for the point.
+     * @param point The point.
+     * @return True if the point is in the rectangle.
+     */
     public static final boolean pointIsInRect(RectF rect, PointF point) {
         return (point.x >= rect.left &&
                 point.x <= rect.right &&
@@ -16,6 +23,7 @@ public class PositionUtils {
     }
 
     /**
+     * Calculate the distance between two points.
      *
      * @param xa Abscissa of the first point (A).
      * @param ya Ordinate of the first point (A).
@@ -30,6 +38,7 @@ public class PositionUtils {
     /**
      * Return the angle between points A and B using trigonometry.<br />
      * A being the center of the unit circle.
+     *
      * @param xa The abscissa of point A.
      * @param ya The ordinate of point A.
      * @param xb The abscissa of point B.
@@ -48,6 +57,7 @@ public class PositionUtils {
     /**
      * Return the angle between points A and B using trigonometry.<br />
      * A being the center of the unit circle.
+     *
      * @param xa The abscissa of point A.
      * @param ya The ordinate of point A.
      * @param xb The abscissa of point B.
@@ -62,6 +72,7 @@ public class PositionUtils {
     /**
      * Return the angle between points A and B using trigonometry.<br />
      * A being the center of the unit circle.
+     *
      * @param xa The abscissa of point A.
      * @param ya The ordinate of point A.
      * @param xb The abscissa of point B.
@@ -70,5 +81,38 @@ public class PositionUtils {
      */
     public static final double angleInDegrees(double xa, double ya, double xb, double yb) {
         return angleInDegrees(xa, ya, xb, yb, false);
+    }
+
+    /**
+     * Convert polar coordinates to cartesian coordinates.
+     *
+     * @param originX The polar abscissa of the pole.
+     * @param originY The polar ordinate of the pole.
+     * @param angle The angle
+     * @param radius The distance from the pole (called the radial coordinate or radius
+     * @param reverse True if the ordinates goes down instead of up. False otherwise.
+     * @return The cartesian coordinates.
+     */
+    public PointF polarToCartesian(float originX, float originY, double angle, float radius, boolean reverse) {
+        float diffX = (float) (radius * Math.cos(angle));
+        float diffY = (float) (radius * Math.sin(angle));
+        if (reverse) {
+            return new PointF(originX + diffX, originY - diffY);
+        }
+        // Standard formula
+        return new PointF(originX + diffX, originY + diffY);
+    }
+
+    /**
+     * Convert polar coordinates to cartesian coordinates.
+     *
+     * @param originX The polar abscissa of the pole.
+     * @param originY The polar ordinate of the pole.
+     * @param angle The angle
+     * @param radius The distance from the pole (called the radial coordinate or radius
+     * @return The cartesian coordinates.
+     */
+    public PointF polarToCartesian(float originX, float originY, double angle, float radius) {
+        return polarToCartesian(originX, originY, angle, radius, false);
     }
 }
