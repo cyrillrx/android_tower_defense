686ad1d128bb88a09e8de6d043b4c748b9e8afc1
Cyril Leroux <cyril0leroux@gmail.com>
2014-03-09 21:48:43 +0100

    Fixed copy_apk.bat.

--- copy_apk.bat
+++ copy_apk.bat
@@ -1,4 +1,5 @@
+echo off
 SET DAY=%date:~0,2%
 SET MONTH=%date:~3,2%
 SET YEAR=%date:~6,4%
-COPY /Y TowerDefense-release.apk "%APK_DIR%\TowerDefense_%YEAR%-%MONTH%-%DAY%.apk"
+COPY /Y TowerDefense\build\apk\TowerDefense-release.apk "%APK_DIR%\TowerDefense_%YEAR%-%MONTH%-%DAY%.apk"
