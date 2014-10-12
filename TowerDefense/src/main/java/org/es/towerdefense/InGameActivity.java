5b83fff4135b773ace4fc0ec8fa9d5c97397a5f7
Cyril Leroux <cyril0leroux@gmail.com>
2014-10-12 23:24:12 +0200

    - Removed android-basic-sample from submodules - Implemented Immersive mode - Updated gradle plugin to 0.13.2 - Implemented manifests placeholders

--- TowerDefense/src/main/java/org/es/towerdefense/InGameActivity.java
+++ TowerDefense/src/main/java/org/es/towerdefense/InGameActivity.java
@@ -1,6 +1,8 @@
 package org.es.towerdefense;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.example.games.basegameutils.BaseGameActivity;
 
@@ -15,6 +17,31 @@ public class InGameActivity extends BaseGameActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_game);
+
+//        View decorView = getWindow().getDecorView();
+//        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+//            @Override
+//            public void onSystemUiVisibilityChange(int visibility) {
+//                boolean visible = (visibility & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) == 0;
+//                //controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
+//            }
+//        });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
+        }
     }
 
     @Override
